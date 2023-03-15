f11e6101abd6acf255fb4aa900c7a3954b68e157
martixjohn <mtxrun@163.com>
2023-03-15 09:08:32 +0800

    add router replace

--- src/services/back/mixins/router.js
+++ src/services/back/mixins/router.js
@@ -3,11 +3,18 @@ import { resolveBackRoutePath } from "@/api/jump"
 export default {
   methods: {
     /**
-     * 跳转后端路由
-     * @param {string} absolutePath 
+     * 跳转后端路由 push
+     * @param {string} absolutePath 绝对路径
      */
     routerPush(absolutePath) {
       this.$router.push(resolveBackRoutePath(absolutePath))
+    },
+    /**
+     * 跳转后端路由 replace
+     * @param {string} absolutePath 
+     */
+    routerReplace(absolutePath){
+      this.$router.replace(resolveBackRoutePath(absolutePath))
     }
   }
 }
