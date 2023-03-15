341b169b49895b2d27b088fc999092e3b117b0de
martixjohn <mtxrun@163.com>
2023-03-15 08:21:39 +0800

    抽离路由配置

---
+++ src/services/back/config/routes/user.js
@@ -0,0 +1,20 @@
+export default {
+    path: 'user',
+    component: () => import('_back/views/user/UserView.vue'),
+    menuName: '用户信息',
+    menuIcon: '',
+    children: [
+        {
+            path: 'detail',
+            component: () => import('_back/views/user/UserDetailView.vue'),
+            menuName: '用户详情',
+            menuIcon: '',
+        },
+        {
+            path: 'test',
+            component: () => import('_back/views/user/UserTestView.vue'),
+            menuName: '用户测试',
+            menuIcon: '',
+        }
+    ]
+}
