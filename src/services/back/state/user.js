62b9cce220bb42393383332442f2c119b686d7b3
martixjohn <mtxrun@163.com>
2023-03-15 17:00:40 +0800

    UserDetailView

--- src/services/back/state/user.js
+++ src/services/back/state/user.js
@@ -1,4 +1,5 @@
 import { goToLoginPage } from "@/api/jump"
+import { dateToString } from "@/utils/date"
 
 const user = {
   namespaced: true,
@@ -6,8 +7,9 @@ const user = {
     username: '2020001001',//
     name: '蔡德一',
     number: '2020001001',//编号 学生号/教职工
-    identify: 1,// 学生1 教师2
-    group: '软件工程学院软件工程专业2020级1班'
+    identify: '学生',// 学生1 教师2
+    group: '软件工程学院软件工程专业2020级1班',
+    entryDate: dateToString(new Date(2020, 8, 1, 0, 0, 0), 'd')
   }),
   mutations: {
 
--- src/services/back/views/user/UserDetailView.vue
+++ src/services/back/views/user/UserDetailView.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container">
-    <el-descriptions class="tables" title="用户详情" :column="3" size="medium" border>
+    <el-descriptions class="tables" title="用户详情" :column="2" size="medium" border>
       <template slot="extra">
-        <el-button type="primary" size="small">操作</el-button>
+        <span>信息有误?&nbsp;</span>
+        <el-button type="primary" size="small" @click="reportWrong">上报错误</el-button>
       </template>
       <el-descriptions-item>
         <template slot="label">
@@ -10,7 +11,7 @@
           姓名
         </template>
         {{ name }}
-        <el-tag>学生</el-tag>
+        <el-tag>{{ identify }}</el-tag>
       </el-descriptions-item>
       <el-descriptions-item>
         <template slot="label">
@@ -29,29 +30,85 @@
       <el-descriptions-item>
         <template slot="label">
           <i class="el-icon-tickets"></i>
-          班级
+          组
         </template>
         软件工程学院软工2020级1班
       </el-descriptions-item>
       <el-descriptions-item>
         <template slot="label">
-          <i class="el-icon-office-building"></i>
-          联系地址
+          入学时间
         </template>
-        江苏省苏州市吴中区吴中大道 1188 号
+        {{ entryDate }}
       </el-descriptions-item>
     </el-descriptions>
-    <div>
-      用户详情
-    </div>
+    <el-descriptions class="security" title="安全" size="medium" border>
+      <el-descriptions-item>
+        <template slot="label">
+          密码
+        </template>
+        <el-button type="primary" size="small">修改密码</el-button>
+      </el-descriptions-item>
+    </el-descriptions>
+    <!-- 上报有误信息 -->
+    <el-dialog title="上报有误信息" :visible.sync="isReportDialogVisible">
+      <el-descriptions :column="2" size="medium" border>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-user"></i>
+            姓名
+          </template>
+          {{ name }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-s-custom"></i>
+            学号
+          </template>
+          {{ number }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-mobile-phone"></i>
+            手机号
+          </template>
+          18100000000
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-tickets"></i>
+            组
+          </template>
+          软件工程学院软工2020级1班
+        </el-descriptions-item>
+      </el-descriptions>
+      <div style="display: flex; justify-content: center; padding-top: 1rem;">
+        <el-button size="small" type="primary">提交修改</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex';
 
 export default {
+  data() {
+    return {
+      isReportDialogVisible: false,
+      report: {
+        name: '',
+        number: '',
+        group: '',
+        entryDate: ''
+      }
+    }
+  },
   computed: {
-    ...mapState('user', ['username', 'name', 'number'])
+    ...mapState('user', ['username', 'name', 'number', 'identify', 'entryDate'])
+  },
+  methods: {
+    reportWrong() {
+      this.isReportDialogVisible = true
+    }
   }
 }
 </script>
@@ -59,6 +116,15 @@ export default {
 <style lang="scss" scoped>
 .container {
   display: flex;
+  width: 100%;
 }
 
+.tables {
+  flex: 1;
+}
+
+.security {
+  flex-basis: 12rem;
+  margin-left: 1rem;
+}
 </style>
